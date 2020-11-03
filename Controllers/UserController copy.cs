e0e7812500f79443f113e258e9186efc60dae7c1
laurin <laurin.weber@students.hevs.ch>
2020-11-03 21:00:11 +0100

    something

--- Controllers/UserController copy.cs
+++ Controllers/UserController copy.cs
@@ -10,74 +10,41 @@ using OpenSundayApi.Models;
 
 namespace OpenSundayApi.Controllers
 {
-    #region LocationsController
+    #region UserController
     [Route("api/[controller]")]
     [ApiController]
-    public class LikesController : ControllerBase
+    public class UserController : ControllerBase
     {
         private readonly OpenSundayContext _context;
 
-        public LikesController(OpenSundayContext context)
+        public UserController(OpenSundayContext context)
         {
             _context = context;
         }
         #endregion
 
-        //get all likes the demanding user has posted
-        // GET: api/Likes
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Like>>> GetLikes()
-        {
-            //get userId from jwt
-             var user_id = User.Claims.First(i => i.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
 
-            return await _context.Like.Where(l => (l.FK_User == (user_id))).ToListAsync();
-        }
-
-    //get all likes from a specific location
-    #region snippet_GetByID_Location
-    // GET: api/Likes/5
+    //check if location has been liked by user
+    #region snippet_GetByID
+    // GET: api/User/5
     [HttpGet("{id}")]
-    public async Task<ActionResult<IEnumerable<Like>>> GetLikes(int id)
+    public async Task<ActionResult<Like>> GetUser(int id)
     {
 
+      var user_id = User.Claims.First(i => i.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
+      
       //get the list of likes from location
       var likes = await _context.Like.Where(l => (l.FK_Location == (id))).ToListAsync();
-
-      if (likes == null)
-      {
-        return null;
-      }
-      return likes;
-    }
-    #endregion
-
-    #region snippet_Create
-    // POST: api/Like (Location Id & isLiked)
-    [HttpPost]
-    public async Task<ActionResult<Like>> PostLike(Like like)
-    {
-
-      //retrieve Userid from Token
-      var user_id = User.Claims.First(i => i.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
-      like.FK_User = user_id;
+      Like like = new Like();
+      like = null;
+        foreach(var l in likes){
+          if(l.FK_User == user_id) {
+            like = l; //if exists return null
+          }
+        }
       
-      var users = await _context.User.Where(u => (u.Id == (user_id))).ToListAsync();
-      //add user to db
-      if(!(users.Count >0)){
-        User u = new User();
-        u.Id = user_id;
-        _context.User.Add(u);
-        await _context.SaveChangesAsync();
-      }
-
-            //Add Like to Db
-            _context.Like.Add(like);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetLikes), new { FK_User = like.FK_User}, like);
+      return like;
     }
     #endregion
-
-    }
+}
 }
