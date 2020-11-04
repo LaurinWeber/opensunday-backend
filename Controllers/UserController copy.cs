5e59361ac98130e93d5e7a78ed7f1bc5611c37c1
laurin <laurin.weber@students.hevs.ch>
2020-11-04 18:25:25 +0100

    favorites

--- Controllers/UserController copy.cs
+++ Controllers/UserController copy.cs
@@ -23,6 +23,24 @@ namespace OpenSundayApi.Controllers
         }
         #endregion
 
+        //get all locations the demanding user has liked (favorites)
+        // GET: api/Likes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Location>>> GetFavorites()
+        {
+            //get userId from jwt
+            var user_id = User.Claims.First(i => i.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
+            
+            var likes = await _context.Like.Where(l => (l.FK_User == (user_id))).ToListAsync();
+            var locations = new List<Location>();
+
+            foreach(var like in likes){
+              var locs = await _context.Location.Where(loc => (loc.Id == (like.FK_Location))).ToListAsync();
+              locations.AddRange(locs);
+            }          
+            return locations;
+        }
+
 
     //check if location has been liked by user
     #region snippet_GetByID
